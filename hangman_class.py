2a1a4845388d3362b8524717cc88e106729defb0
Marcin Stodolny <marcin.stodolny@o2.pl>
2022-09-07 14:28:32 +0200

    formated code by black

--- hangman_class.py
+++ hangman_class.py
@@ -13,7 +13,9 @@ class hangman:
         self.user_choice = ""
 
     def read_words_from_file(self):
-        with open(str(Path(__file__).parent.absolute()) + '\\' + "countries.txt") as words:
+        with open(
+            str(Path(__file__).parent.absolute()) + "\\" + "countries.txt"
+        ) as words:
             self.word_list = words.readlines()
 
     def main(self):
@@ -24,7 +26,9 @@ class hangman:
 
     def get_menu_input_and_check(self):
         while self.user_choice not in ["1", "2", "3"]:
-            self.user_choice = input("1. Easy\n2. Medium\n3. Hard\nq. Quit\nChose dificulty(1/2/3/quit): ").lower()
+            self.user_choice = input(
+                "1. Easy\n2. Medium\n3. Hard\nq. Quit\nChose dificulty(1/2/3/quit): "
+            ).lower()
             self.valid_menu_input_or_quit()
 
     def valid_menu_input_or_quit(self):
@@ -33,7 +37,7 @@ class hangman:
             sys.exit()
         elif self.user_choice not in ["1", "2", "3"]:
             print("Invalid input value, try again")
-            
+
     def set_difficulty_level(self):
         match self.user_choice:
             case "1":
@@ -44,9 +48,9 @@ class hangman:
                 self.lives = 8
             case "3":
                 self.difficulty = self.word_list[174:242]
-                self.lives = 10    
+                self.lives = 10
         self.show_lives_and_ussed_letters()
-            
+
     def create_word_and_hide_it(self):
         self.word = random.choice(self.difficulty)[:-1]
         self.create_word_with_underscores(list(self.word))
@@ -76,15 +80,19 @@ class hangman:
         if self.user_choice == "quit":
             print("Good-bye")
             sys.exit()
-        
+
     def check_if_user_input_is_correct_or_repeat(self):
-        while self.user_choice in self.used_letters or self.user_choice not in self.letters_lowercase or self.user_choice == "":
+        while (
+            self.user_choice in self.used_letters
+            or self.user_choice not in self.letters_lowercase
+            or self.user_choice == ""
+        ):
             self.show_lives_and_ussed_letters()
             self.letter_already_on_list_check()
             self.entered_invalid_value_check()
             self.get_user_input_or_quit()
         self.used_letters.append(self.user_choice)
-        
+
     def letter_already_on_list_check(self):
         if self.user_choice in self.used_letters:
             print("This letter was already used")
@@ -96,7 +104,11 @@ class hangman:
             self.show_state_of_hidden_word()
 
     def check_if_letter_is_in_the_hidden_word(self):
-        found_index = [item for item, found in enumerate(self.word) if self.user_choice == found.lower()]
+        found_index = [
+            item
+            for item, found in enumerate(self.word)
+            if self.user_choice == found.lower()
+        ]
         self.convert_underscore_into_valid_letter(found_index)
         if not found_index:
             self.lives -= 1
@@ -104,7 +116,7 @@ class hangman:
     def convert_underscore_into_valid_letter(self, found_index):
         for item in found_index:
             self.word_to_guess[item] = self.word[item]
-            
+
     def check_if_the_game_is_lost(self):
         if self.lives <= 0:
             self.show_hangman_picture()
@@ -121,46 +133,59 @@ class hangman:
 
     def show_hangman_picture(self):
         hangman = [
-            '''    +---+
+            """    +---+
     O   |
    /|\  |
    / \  |
-       ===''', '''
+       ===""",
+            """
     +---+
     O   |
    /|\  |
    /    |
-       ===''', '''
+       ===""",
+            """
     +---+
     O   |
    /|\  |
         |
-       ===''', '''
+       ===""",
+            """
     +---+
     O   |
    /|   |
         |
-       ===''', '''
+       ===""",
+            """
     +---+
     O   |
     |   |
         |
-       ===''', '''
+       ===""",
+            """
     +---+
     O   |
         |
         |
-       ===''', '''
+       ===""",
+            """
     +---+
         |
         |
         |
-       ===''', '''    
+       ===""",
+            """    
         |
         |
         |
-       ===''', '''
-       ===''', '', '', '', '', ''
+       ===""",
+            """
+       ===""",
+            "",
+            "",
+            "",
+            "",
+            "",
         ]
         print(hangman[self.lives])
 
